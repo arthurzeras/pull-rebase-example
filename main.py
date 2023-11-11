01815608aa90d8cc2a80ce43c4f3793c5e84c2c8
Example <example@gmail.com>
2023-11-11 14:43:05 -0300

    feat: create azure integration

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 class Integration:
-    def cloud_integrations():
+    def cloud_integrations(self, cloud):
+        if cloud == 'azure':
+            self.azure_integration()
+
         print('cloud integrations')
+
+    def azure_integration(self):
+        print('azure integration')
 
     def aws_integration():
         print('aws integration')
